490b374be60a0c2cb92413359d59a7a2a6b1f3b8
Tên của bạn <hdat11320@gmail.com>
2024-12-05 14:08:27 +0700

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    async rewrites() {
+        return [
+            {
+                source: '/old-route',  // Đường dẫn cũ
+                destination: '/new-route',  // Đường dẫn mới
+            },
+            // Bạn có thể thêm nhiều rewrites khác ở đây
+        ];
+    },
 };
 
 export default nextConfig;
