eccebf9837eddd8a85d08829d0f62a5a90eaf711
Tên của bạn <hdat11320@gmail.com>
2024-12-05 14:24:13 +0700

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/old-route',  // Đường dẫn cũ
-                destination: '/new-route',  // Đường dẫn mới
-            },
-            // Bạn có thể thêm nhiều rewrites khác ở đây
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
